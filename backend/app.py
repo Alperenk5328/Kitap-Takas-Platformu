a4d77326b1d6a5dd9345a1dd1674feba3537cb68
Alperenk5328 <karaalperen7@gmail.com>
2026-05-13 13:18:43 +0300

    deneme3

--- backend/app.py
+++ backend/app.py
@@ -395,7 +395,8 @@ def swap_address():
         'both_ready': both_ready  # her iki taraf da gönderdiyse karşılıklı bilgiler açılır
     })
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, port=5000)
